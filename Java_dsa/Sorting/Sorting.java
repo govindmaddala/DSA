d7c02ecbe49f10f3dd240545eb53ccee887898bd
GOvind MADdala <99980734+govindmaddala@users.noreply.github.com>
2024-08-03 20:12:31 +0530

    max profit is added

--- Java_dsa/Sorting/Sorting.java
+++ Java_dsa/Sorting/Sorting.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Sorting {
@@ -43,7 +44,8 @@ public class Sorting {
         for (int i = 0; i < n - 1; i++) {
             int min = arr[i];
             int minIndex = i;
-            //get the minimum index and minmum next and swap with the sorted arr's last index with min num we get from unsorted array.
+            // get the minimum index and minmum next and swap with the sorted arr's last
+            // index with min num we get from unsorted array.
             for (int j = i + 1; j < n; j++) {
                 int current = arr[j];
                 if (current < min) {
@@ -57,6 +59,38 @@ public class Sorting {
         return arr;
     }
 
+    public static int[] merge2SortedArr(int[] arr1, int arr2[]) {
+        int m = arr1.length;
+        int n = arr2.length;
+        int[] arr3 = new int[m + n];
+        int x = 0; // for arr1
+        int y = 0; // for arr2
+        int mergedIndex = 0;
+        while (x < m && y < n) {
+            if (arr1[x] <= arr2[y]) {
+                arr3[mergedIndex] = arr1[x];
+                mergedIndex++;
+                x++;
+            } else {
+                arr3[mergedIndex] = arr2[y];
+                mergedIndex++;
+                y++;
+            }
+
+        }
+        while (x < m) {
+            arr3[mergedIndex] = arr1[x];
+            x++;
+        }
+        while (y < n) {
+            arr3[mergedIndex] = arr2[y];
+            y++;
+        }
+        Arrays.stream(arr3).forEach(a -> System.out.print(a + " "));
+        return arr3;
+
+    }
+
     public static void main(String[] args) {
         int[] arr1 = { 10, 1, 5, 3, 9, 2, 6, 8, 0, -1, 1 };
         int[] arr2 = { 10, 1, 5, 3, 9, 2, 6, 8, 0, -1, 1 };
@@ -65,11 +99,14 @@ public class Sorting {
         arr1 = Sorting.bubbleSort(arr1);
         arr2 = Sorting.insertionSort(arr2);
         arr3 = Sorting.selectionSort(arr3);
+        
         Arrays.stream(arr1).forEach(x -> System.out.print(x + " "));
         System.out.println("\n----------------------------------");
         Arrays.stream(arr2).forEach(x -> System.out.print(x + " "));
         System.out.println("\n----------------------------------");
-        Arrays.stream(arr3).forEach(x -> System.out.print(x + " "));
+        Sorting.merge2SortedArr(arr1, arr2);
         System.out.println("\n----------------------------------");
+        // Arrays.stream(arr3).forEach(x -> System.out.print(x + " "));
+        // System.out.println("\n----------------------------------");
     }
 }
